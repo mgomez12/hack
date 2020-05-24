4e97878971e04a5bb6d1bfe69241c00f678d1b89
Ayush Agarwal <ayush0316@gmail.com>
2020-05-24 03:38:06 -0500

    addingcss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import {Input, Button, Image, Header} from 'semantic-ui-react';
-import {CSSTransition} from 'react-transition-group';
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React, { useState, useEffect } from 'react';
+import { Input, Button, Image, Header } from 'semantic-ui-react';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 
 function App() {
@@ -26,9 +27,9 @@ function App() {
     // setCookie(null);
   }
 
-  function hashString(str){
+  function hashString(str) {
     let hash = 0;
-    for(let i = 0; i < str.length; i++){
+    for (let i = 0; i < str.length; i++) {
       hash += Math.pow(str.charCodeAt(i) * 31, str.length - i);
       hash = hash & hash;
     }
@@ -37,83 +38,75 @@ function App() {
 
   const getData = () => {
     const promise1 = fetch("https://api.nationalize.io?name=" + name).then((data) => data.json())
-    .then(data => {
-      console.log(data);
-      if (data.country.length == 0) {
-        return Promise.reject();
-      }
-      else {
-        setCode(data.country[0].country_id)
-        return fetch(`https://restcountries.eu/rest/v2/alpha/${data.country[0].country_id}`)
-        .then(res => res.json())
-        .then(data => setCountryName(data.name));
-        
-      }
-    })
+      .then(data => {
+        console.log(data);
+        if (data.country.length == 0) {
+          return Promise.reject();
+        }
+        else {
+          setCode(data.country[0].country_id)
+          return fetch(`https://restcountries.eu/rest/v2/alpha/${data.country[0].country_id}`)
+            .then(res => res.json())
+            .then(data => setCountryName(data.name));
+
+        }
+      })
 
     const promise3 = fetch("http://fortunecookieapi.herokuapp.com/v1/cookie")
-    .then(data => data.json())
-    .then(data => {
-      console.log(data);
-      setCookie(data[0]);
-    });
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        setCookie(data[0]);
+      });
 
     const promise2 = fetch("https://xkcd.now.sh/?comic=latest").then((data) => data.json())
-    .then(data => {
-      console.log(data.num);
-      console.log(hashString(name));
-      const id = hashString(name) % data.num;
-      fetch(`https://xkcd.now.sh/?comic=${id}`).then((data) => data.json())
       .then(data => {
         setComicURL(data);
-      })
-    });
+      });
     Promise.all([promise1, promise2, promise3]).then(() => setReady(true), () => setError(true));
   }
-    return (
-      <div className="App">
-        <CSSTransition
-          unmountOnExit
-          in={!ready}
-          classNames="title"
-          timeout={2000}>
-          <div className="centered">
-            <div className="title">
-              ABBABHABA.io
+  return (
+    <div className="App">
+      <CSSTransition
+        unmountOnExit
+        in={!ready}
+        classNames="title"
+        timeout={2000}>
+        <div className="centered">
+          <div className="title">
+            ABBABHABA.io
             </div>
-            <Input placeholder="Enter your name..." onChange={update}/>
-            <br/>
-            <Button style={{margin:'10px'}} basic color='violet' disabled={name===""} onClick={getData}>
-              Submit!
-            </Button>
-            {error ? <div>You have a fucking stupid name</div> : ''}
+          <div class="ui action input">
+            <Input opacity=".6" placeholder="Enter your name..." onChange={update} />
+            <button opacity=".6" class="ui button" disabled={name === ""} onClick={getData} >Submit!</button>
           </div>
-        </CSSTransition>
-        <CSSTransition
-          in={ready}
-          classNames="result"
-          timeout={2000}
-          unmountOnExit>
-            <div className="absolute-pos">
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={ready}
+        classNames="result"
+        timeout={2000}
+        unmountOnExit>
+        <div className="absolute-pos">
           <Button onClick={reset}> Start Over</Button>
-          
-            <Image src={`https://www.countryflags.io/${code.toLowerCase()}/shiny/64.png`} />
-            {countryName}
-            <Header>
-              XKCD #{comicURL.num}
-            </Header>
-            <Image src={`${comicURL.img}`} />
-            {comicURL.alt}
-            <Header>Fortune Cookie</Header>
+
+          <Image src={`https://www.countryflags.io/${code.toLowerCase()}/flat/64.png`} />
+          {countryName}
+          <Header>
+            XKCD #{comicURL.num}
+          </Header>
+          <Image src={`${comicURL.img}`} />
+          {comicURL.alt}
+          <Header>Fortune Cookie</Header>
             Fortune: {cookie === null ? '' : cookie.fortune.message}
-            <br/>
+          <br />
             Lotto: {cookie === null ? '' : cookie.lotto.numbers.map((num) => num + ', ')}
-            <br/>
+          <br />
             Lesson: {cookie === null ? '' : cookie.lesson.translation}
-          </div>
-        </CSSTransition>
-      </div>
-    );
+        </div>
+      </CSSTransition>
+    </div>
+  );
 }
 
 export default App;
