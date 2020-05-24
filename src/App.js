029dd07d21f450b8a4f6d2eaabe197018801163f
Miguel Gomez <mgomez224758@gmail.com>
2020-05-23 22:56:58 -0500

    sorta working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import {Header, Icon} from 'semantic-ui-react';
+import React, {useState, useEffect} from 'react';
+import {Input, Button, Image} from 'semantic-ui-react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    <Header as='h2' icon>
-    <Icon name='settings' />
-    Account Settings
-    <Header.Subheader>
-      Manage your account settings and set e-mail preferences.
-    </Header.Subheader>
-  </Header>
-    
-    </div>
-  );
+  const [name, setName] = useState("");
+  const [ready, setReady] = useState(false);
+  const [code, setCode] = useState("");
+
+  const update = (event, data) => {
+    setName(data.value)
+  }
+
+
+  const getData = () => {
+    fetch("https://api.nationalize.io?name=" + name).then((data) => data.json())
+    .then(data => {
+      console.log(data.country);
+      setCode(data.country[0].country_id)
+      setReady(true);
+    })
+  }
+    return (
+      <div className="App">
+        {!ready ? 
+        <div className="centered">
+        <div className="title">
+          ABBABHABA.io
+        </div>
+        <Input placeholder="Enter your name..." onChange={update}/>
+        <br/>
+        <Button style={{margin:'10px'}} basic color='violet' disabled={name===""} onClick={getData}>
+          Submit!
+          </Button>
+        </div>
+      :
+      <Image src={`https://www.countryflags.io/${code.toLowerCase()}/shiny/64.png`} />
+        }
+      </div>
+    );
 }
 
 export default App;
