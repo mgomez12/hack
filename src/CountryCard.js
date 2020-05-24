8f55c4cdd6a4e6995059850179ff05a72d888654
rbnoeske <rbnoeske@protonmail.com>
2020-05-24 11:04:40 -0500

    Visual Fixes

--- src/CountryCard.js
+++ src/CountryCard.js
@@ -8,7 +8,7 @@ return(
     <Card style={{height:'100%', width:'100%'}}>
         <Card.Content>
         <div className="card-title">Nationality</div>
-        <Header style={{height:"30%", margin:"15px"}}> Based on your name, we believe you are from:</Header>
+        <Header style={{height:"1%", margin:"15px"}}> Based on your name, we believe you are from:</Header>
         <Image src={`https://www.countryflags.io/${props.code.toLowerCase()}/flat/64.png`} />
         <Header size="large" style={{margin:"15px"}}>{props.countryName}</Header>
         </Card.Content>
